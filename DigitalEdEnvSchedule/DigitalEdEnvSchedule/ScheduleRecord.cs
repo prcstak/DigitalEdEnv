4f0cdb561ed59e454b3b8b5a6e757e43b56407c5
Marsel Gilyazetdinov <g.marsel2002@yandex.ru>
2023-05-28 17:47:20 +0300

    add schedule

---
+++ DigitalEdEnvSchedule/DigitalEdEnvSchedule/ScheduleRecord.cs
@@ -0,0 +1,3 @@
+﻿namespace DigitalEdEnvSchedule;
+
+public record ScheduleRecord(String Lesson, String Group, int Room, int LessonNum);
---
+++ DigitalEdEnvSchedule/DigitalEdEnvSchedule/Values.cs
@@ -0,0 +1,10 @@
+﻿namespace DigitalEdEnvSchedule;
+
+public static class Values
+{
+    public static List<String> Lessons = new(){"Ин. яз.", "Дискр. мат."};
+
+    public static List<String> Groups = new() {"11-001", "11-006", "11-012"};
+
+    public static List<int> Room = new() {809, 1309, 1509};
+}
